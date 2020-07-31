8ede7952c0bbfa120c2591b0c3e9392358c217b0
ArthurHlt <arthur.halet@orange.com>
2020-07-31 16:13:11 +0200

    allow dash in word

--- vendor/github.com/ArthurHlt/grok/patterns.go
+++ vendor/github.com/ArthurHlt/grok/patterns.go
@@ -12,7 +12,7 @@ var patterns = map[string]string{
 	"BASE16NUM":          `(0[xX]?[0-9a-fA-F]+)`,
 	"POSINT":             `\b(?:[1-9][0-9]*)\b`,
 	"NONNEGINT":          `\b(?:[0-9]+)\b`,
-	"WORD":               `\b\w+\b`,
+	"WORD":               `\b(\w|-)+\b`,
 	"NOTSPACE":           `\S+`,
 	"SPACE":              `\s*`,
 	"DATA":               `.*?`,
